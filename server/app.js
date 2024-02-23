893e0bd5734caa86c4b8bdf6f38436744bfc7c70
Alfredo <alfredo.galvez1996@gmail.com>
2024-02-23 13:47:05 +0000

    Update - Connection to test, fixed.

--- server/app.js
+++ server/app.js
@@ -1,31 +1,17 @@
-const client = require('../server/connection')
+const client = require("../server/connection");
 const express = require("express");
 const app = express();
 const {
   fetchingCities,
   fetchingCityToilets,
-  patchingCityToilets
-
+  patchingCityToilets,
 } = require("./controllers/controllers");
 
 app.get("/api/cities", fetchingCities);
 
-app.get("/api/:city_name/toilets", fetchingCityToilets); //add property comments number and rating number 
+app.get("/api/:city_name/toilets", fetchingCityToilets); //add property comments number and rating number
 
-app.patch("/api/:city_name/toilets/:toiletId", patchingCityToilets)
-
-
-
-PATCH api/toiletid
-GET /api/toiletid/comments
-
-comments : strings of all the comment
-
-
-POST api/toiletid/comment
-
-posting a brand new insertone into the comments collection
-
+app.patch("/api/:city_name/toilets/:toiletId", patchingCityToilets);
 
 app.use((err, req, res, next) => {
   if (err.status === 404 && err.msg === "City not found") {
